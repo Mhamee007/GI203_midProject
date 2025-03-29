4b3a6605dab774488448731d3c09c755b70848f9
Mhamee <Mhameework@gmail.com>
2025-03-29 22:30:49 +0700

    Re1.0

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,31 +7,44 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private bool isGrounded;
     private bool isAir;
+    
     private float respawnDelay = 2f;
+    [SerializeField]float speed = 2.5f;
+    [SerializeField] float jumpForce = 25f;
 
+<<<<<<< HEAD
     [SerializeField] float speed = 5f;
     [SerializeField] float jumpForce = 30f;
     float gravity = Physics.gravity.magnitude;
+=======
+>>>>>>> parent of dc9c805 (UpdatePlayerContro)
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-    void Update()
+
+    void Update() 
     {
         //Player movement----------------------------------------------------
 
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
+<<<<<<< HEAD
         Vector3 move = new Vector3(moveX, 0, moveZ) * speed;//speed walk
         rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);//spin of player
+=======
+        Vector3 move = new Vector3(moveX, 0, moveZ) * speed;
+        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
+>>>>>>> parent of dc9c805 (UpdatePlayerContro)
 
         if (Input.GetKey(KeyCode.Space) && isGrounded == true && isAir == false) //Jumping
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
+
     }
 
     // On ground and Air cheeck---------------------------------------------
@@ -55,5 +68,5 @@ public class PlayerController : MonoBehaviour
     {
         isAir = true;
     }
-
+    
 }
